4170de887ce488183dd3f05ab2e53e1f2d31bc17
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-06 15:59:55 +0300

    Adds save datail method  that save the user object to the user list

--- pass.py
+++ pass.py
@@ -9,3 +9,11 @@ class User:
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+        
+    def save_detail(self):
+        '''
+        class method that saves the user object into the user_list
+        '''    
+        
+        self.user_list.append(self)
+ 
