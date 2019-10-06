763b47d68d299ec84e54065890821febc7cc41c6
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-06 17:53:37 +0300

    Adds indentation to the file

--- app.py
+++ app.py
@@ -4,25 +4,29 @@ from passw import User
 from passw import Credential
 import random
 
-def create_credential(name,password):
+
+def create_credential(name, password):
     '''
     Function that create  new account and the credentials
     '''
-    new_credential = Credential(name,password)
+    new_credential = Credential(name, password)
     return new_credential
 
-def save_credential(credentials): 
+
+def save_credential(credentials):
     '''
     Function to save the new credential details created
     '''
     credentials.save_credential()
-    
+
+
 def find_credential(account_name):
     '''
     Function that finds credentials by account name
-    '''    
-    
+    '''
+
     return Credential.find_by_name(account_name)
+
 
 def existing_credentials(name):
     '''
@@ -30,11 +34,13 @@ def existing_credentials(name):
     '''
     return Credential.credential_exists(name)
 
+
 def delete_credential(credentials):
     '''
     Function that deletes credentials that are no longer in use
     '''
     return credentials.delete_credential(credentials)
+
 
 def display_credential():
     '''
