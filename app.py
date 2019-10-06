adb65d7c2522e76d67a9c3b169b239f500d66b02
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-06 17:18:31 +0300

    Adds a function that creates new account and the credentials

--- app.py
+++ app.py
@@ -3,3 +3,10 @@
 from passw import User
 from passw import Credential
 import random
+
+def create_credential(name,password):
+    '''
+    Function that create  new account and the credentials
+    '''
+    new_credential = Credential(name,password)
+    return new_credential
