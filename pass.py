b4279018551c8f8c9e42532921f498cfd01f93ba
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-06 15:38:32 +0300

    Adds a constructor that instantiate the user objects and a list to store the objects

--- pass.py
+++ pass.py
@@ -3,3 +3,9 @@ class User:
     '''
     Class that generates new instances of users.
     '''
+    
+    user_list = []
+    
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
