5cb739d9b8606fc8927bbf8bb90bcdb4c16de678
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-07 08:06:36 +0300

    Adds a  test_init test case to test if the object is intialized

--- pass_test.py
+++ pass_test.py
@@ -3,6 +3,7 @@ import unittest
 from passw import User
 from passw import Credential
 
+
 class TestPassword(unittest.TestCase):
     '''
     Test class that defines test cases for the application behaviours.
@@ -10,22 +11,34 @@ class TestPassword(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in
         creating test cases
 
-    ''' 
-    
+    '''
+
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        
-        self.new_user = User('daniel','123')
-        self.new_credential = Credential('twiter','456')
-    
+
+        self.new_user = User('daniel', '123')
+        self.new_credential = Credential('twiter', '456')
+
     def tearDown(self):
         '''
         tearDown method that does clean up after each test case has run
-        '''   
-        
+        '''
+
         User.user_list = []
-        Credential.credentials_list =[]  
-    
-        
+        Credential.credentials_list = []
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is intialized
+        '''
+
+        self.assertEqual(self.new_user.user_name, "daniel")
+        self.assertEqual(self.new_user.password, "123")
+        self.assertEqual(self.new_credential.account_name, "twiter")
+        self.assertEqual(self.new_credential.account_password, "456")
+
+
+if __name__ == '__main__':
+    unittest.main()
