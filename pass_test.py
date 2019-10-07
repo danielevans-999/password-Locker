13d4b8daf292e38585ef55dfc44fff9eaea2b8b7
danielevans-999 <danielevans.karani@gmail.comgit>
2019-10-07 07:50:41 +0300

    Adds  Test class that defines test cases for the application behaviours.

--- pass_test.py
+++ pass_test.py
@@ -3,3 +3,11 @@ import unittest
 from passw import User
 from passw import Credential
 
+class TestPassword(unittest.TestCase):
+    '''
+    Test class that defines test cases for the application behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in
+        creating test cases
+
+    ''' 
